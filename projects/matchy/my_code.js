aa4f5b2a9ef8ea4cbb3dd47497761f2e787a1bd4
wrford <wrford@gmail.com>
2016-03-02 01:03:43 +0000

    first step of matchy

---
+++ projects/matchy/my_code.js
@@ -0,0 +1,17 @@
+
+// Open up my_code.js
+//  Create a variable named animal and assign it to an empty object.
+var animal = {};
+
+//  Using dot notation give animal a property named species with a value of any animal species.
+animal.species = 'Okapi johnstoni';
+
+//  Using bracket notation give animal a property called name with a value of your animal`s name.
+animal['name'] = 'Bernard';
+
+//  Using either notation, give animal a property called noises with a value of empty array.
+animal.noises = [];
+
+//  run console.log(animal) and refresh your page to see what animal looks like.
+console.log(animal);
+//  It should be something like:
