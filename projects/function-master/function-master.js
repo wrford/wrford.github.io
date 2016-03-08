8c7254e8b7a35c028586ab0aa71cac983c1b272a
wrford <wrford@gmail.com>
2016-03-08 03:04:34 +0000

    first 7 functions passing

--- projects/function-master/function-master.js
+++ projects/function-master/function-master.js
@@ -1,1 +1,76 @@
+// Should take an object and return its values in an array
+function objectValues(obj){
+    var output = [];
+    for (var key in obj){
+        output.push(obj[key]);
+    }
+    return output;
+}
 
+// keysToString() : Should take an object and return all its keys in a string each separated with a space 
+function keysToString(obj){
+    return Object.keys(obj).join(' ');
+}
+
+// function keysToString(obj){
+//     var output = '';
+//     for (var key in obj){
+//         if (obj.key ===obj[obj.length-1]){
+//             output += key;
+//         }
+//         else
+//         output += key;
+//         output += ' ';
+        
+//     }
+//     return output;
+// }
+
+// valuesToString() : Should take an object and return all its string values in a string each separated with a space
+function valuesToString(obj){
+    var output = '';
+    for (var key in obj){
+        if(typeof obj[key] === 'string'){
+        output += obj[key];
+        output += ' ';
+        };
+    }
+    return output.trim();
+    
+   // return objectValues(obj).toString().join(' ');
+}
+
+
+//arrayOrObject() : Should take one argument and return 'array' if its an array and 'object' if its an object 
+function arrayOrObject(arg1){
+    if (Array.isArray(arg1)) return 'array';
+    else  return 'object';
+}
+
+// capitalizeWord() : Should take a string of one word, and return the word with its first letter capitalized
+function capitalizeWord(str){
+    var firstChar = str.substr(0,1);
+    // or word[0]
+    var rest = str.substr(1);
+    return firstChar.toUpperCase() + rest;
+}
+
+function capitalizeAllWords(str){
+    var split = str.split(' ');
+    
+    var out = '';
+    
+    for (var i =0;i<split.length; i++){
+        out += capitalizeWord(split[i]) + ' ';
+    }
+    return out.trim();
+}
+
+
+// welcomeMessage() : Should take an object with a name property and return 'Welcome <Name>!'
+
+function welcomeMessage(obj){
+    
+    var output = 'Welcome' + ' ' + capitalizeWord(obj.name) + '!';
+    return output.trim();
+}
